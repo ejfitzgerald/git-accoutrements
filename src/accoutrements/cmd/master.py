51e93cc3b49eefb511da21b44342386c76c4b4f8
Ed FitzGerald <ejafitzgerald@gmail.com>
2021-05-31 22:37:40 +0100

    feat: add support for non-master branch names

--- src/accoutrements/cmd/master.py
+++ src/accoutrements/cmd/master.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import re
 import subprocess
 
 from accoutrements import detect_upstream_remote
@@ -9,6 +10,29 @@ def parse_commandline():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--fetch', action='store_true', help='Fetch the lastest updates from the remote')
     return parser.parse_args()
+
+
+def detect_master_branch(remote: str) -> str:
+    matcher = re.compile(r'^\s+(\w+)/(\w+)')
+
+    remote_branches = set()
+
+    # attempt to detect
+    cmd = ['git', 'branch', '--list', '-r']
+    for line in subprocess.check_output(cmd).decode().splitlines():
+        match = matcher.search(line)
+        assert match is not None
+
+        # split the remote name
+        branch_remote, branch_name = match.groups()
+        if branch_remote == remote:
+            remote_branches.add(branch_name)
+
+    for master_name in ('master', 'main', 'trunk'):
+        if master_name in remote_branches:
+            return master_name
+
+    raise RuntimeError(f'Unable to detect master branch name: {",".join(list(sorted(remote_branches)))}')
 
 
 def main():
@@ -22,6 +46,9 @@ def main():
         cmd = ['git', 'fetch', remote, '-p']
         subprocess.check_call(cmd)
 
+    # detect the master branch name
+    master_name = detect_master_branch(remote)
+
     # create the new branch
-    cmd = ['git', 'checkout', '-B', 'master', f'{remote}/master']
+    cmd = ['git', 'checkout', '-B', master_name, f'{remote}/{master_name}']
     subprocess.check_call(cmd)
